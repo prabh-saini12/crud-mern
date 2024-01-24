f88005b23bd2a9f1e99d2551ff6dbbdf16fb9804
khushiram saini <prabhakarsaini800@gmail.com>
2024-01-24 18:25:35 +0530

    server setup

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectToMongo = require('./db');
+const port = process.env.PORT;
+connectToMongo();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log("server running");
+});
